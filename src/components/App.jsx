8f6bbf0bfc20a4659b75a3ec6ab5adc9957288c8
Soham Patel <soham.patel2808@gmail.com>
2022-05-07 21:28:50 +0530

    dynamically render notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,12 +5,25 @@ import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
 
+import { mockNotes } from '../mock-data/notes';
+
 class App extends React.Component {
+  createNote(note) {
+    return (
+      <Note 
+        key={note.key}
+        title={note.title}
+        content={note.content} />
+    );
+  }
+
   render() {
     return (
       <React.Fragment>
         <Header />
-        <Note />
+        
+        {mockNotes.map(this.createNote)}
+
         <Footer />
       </React.Fragment>
     );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -5,8 +5,8 @@ class Note extends React.Component {
   render() {
     return (
       <div className='note'>
-        <h1>Note title</h1>
-        <p>Note content</p>
+        <h1>{this.props.title}</h1>
+        <p>{this.props.content}</p>
       </div>
     );
   }
